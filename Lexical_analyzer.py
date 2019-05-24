bcdf25df2f1a79cdcb6d2faf5a25db6ef2340697
Rozhiin <nobahari.r@gmail.com>
2019-05-24 11:15:49 +0430

    get_next_token is now! really giving the next token :))))

--- Lexical_analyzer.py
+++ Lexical_analyzer.py
@@ -1,8 +1,8 @@
 import string
 
-file_name = 'testcase_5.txt'
+file_name = 'testcase_3.txt'
 input_file = open(file_name, mode='r')
-code_string_all = input_file.readlines()
+code_string_all = input_file.read()
 
 scanner_file_name = './scanner.txt'
 scanner_file = open(scanner_file_name, mode='w')
@@ -10,10 +10,8 @@ lexical_file_name = './lexical_errors.txt'
 error_file = open(lexical_file_name, mode='w')
 
 code_pointer = 0
-token = []
 
-scanner_line_iterator = 1
-lexical_error_line_iterator = 1
+code_line = 1
 symbols = ['{', '}', ';', ':', ',', '(', ')', '&', '*', '=', '+', '%', '^', '|', '/', '>', '<', '~', '[', '[', '"',
            "'", '-', '?']
 dict
@@ -39,14 +37,17 @@ error_list = []
 
 
 def SymNumIdKeySpace(not_used):
-    global new_state, code_pointer, valid_token
-    char = code_string[code_pointer]
+    global new_state, code_pointer, valid_token, code_line
+    char = code_string_all[code_pointer]
     code_pointer += 1
     if char in symbols:
         new_state = ['symbol', char]
         return char
 
     if char in space:
+        if char == '\n':
+            code_line += 1
+
         new_state = ['end_of_token', ' ']
         return ''
 
@@ -58,7 +59,7 @@ def SymNumIdKeySpace(not_used):
         new_state = ['IdKey', ' ']
         return char
 
-    if char == '!' and code_pointer < len(code_string) and code_string[code_pointer] == '=':
+    if char == '!' and code_pointer < len(code_string_all) and code_string_all[code_pointer] == '=':
         code_pointer += 1
         new_state = ['symbol', '!=']
         return '!='
@@ -68,8 +69,8 @@ def SymNumIdKeySpace(not_used):
 
 
 def IdKey(not_used):
-    global new_state, code_pointer, valid_token
-    char = code_string[code_pointer]
+    global new_state, code_pointer, valid_token, code_line
+    char = code_string_all[code_pointer]
 
     if (char in space) or (char in symbols):
         new_state = ['end_of_token', ' ']
@@ -85,14 +86,14 @@ def IdKey(not_used):
 
 
 def number(not_used):
-    global new_state, code_pointer, valid_token
-    char = code_string[code_pointer]
+    global new_state, code_pointer, valid_token, code_line
+    char = code_string_all[code_pointer]
 
     if (char in space) or (char in symbols):
         new_state = ['end_of_token', ' ']
         return ''
 
-    if char in digit or char==".":
+    if char in digit or char == ".":
         code_pointer += 1
         return char
 
@@ -104,13 +105,13 @@ def number(not_used):
 def symbol(pre_char):
     token = ''
     global code_pointer, new_state
-    char = code_string[code_pointer]
+    char = code_string_all[code_pointer]
     if char in symbol_permutations.get(pre_char):
         token += char
         code_pointer += 1
 
-        if code_pointer < len(code_string) and (char == '>' or char == '<'):
-            char = code_string[code_pointer]
+        if code_pointer < len(code_string_all) and (char == '>' or char == '<'):
+            char = code_string_all[code_pointer]
             if char == '=':
                 token += char
                 code_pointer += 1
@@ -128,10 +129,11 @@ def symbol(pre_char):
 
 
 def one_line_comment(not_used):
-    global code_string, code_pointer, new_state
-    char = code_string[code_pointer]
+    global code_string_all, code_pointer, new_state, code_line
+    char = code_string_all[code_pointer]
     code_pointer += 1
     if char == '\n':
+        code_line += 1
         new_state = ['end_of_token']
         return ''
 
@@ -139,13 +141,15 @@ def one_line_comment(not_used):
 
 
 def multi_line_comment(not_used):
-    global code_string, code_pointer, new_state, valid_token
+    global code_string_all, code_pointer, new_state, valid_token, code_line
     valid_token = False
-    char = code_string[code_pointer]
+    char = code_string_all[code_pointer]
+    if char == '\n':
+        code_line += 1
     code_pointer += 1
     if char == '*':
-        if code_pointer < len(code_string):
-            if code_string[code_pointer] == '/':
+        if code_pointer < len(code_string_all):
+            if code_string_all[code_pointer] == '/':
                 valid_token = True
                 code_pointer += 1
                 new_state = ['end_of_token', ' ']
@@ -153,15 +157,24 @@ def multi_line_comment(not_used):
     return ''
 
 
-def get_next_token(init_state):
-    global token_list, valid_token, error_list, new_state
-    acceptable_state = [[None, ' ']]
-    new_state = init_state
-    token = ''
+pre_line_state = 'SymNumIdKeySpace'
 
-    while code_pointer < len(code_string):
+
+def make_next_token():
+    global token_list, valid_token, error_list, new_state, pre_line_state
+    if code_pointer < len(code_string_all):
+        if pre_line_state == 'multi_line_comment':
+            init_state = ['multi_line_comment', ' ']
+        else:
+            init_state = ['SymNumIdKeySpace', ' ']
+
+        acceptable_state = [[None, ' ']]
+        new_state = init_state
+        valid_token = True
+        token = ''
+
         while new_state[0] != 'end_of_token':
-            if code_pointer < len(code_string):
+            if code_pointer < len(code_string_all):
                 acceptable_state = new_state
                 token += eval(acceptable_state[0] + "('" + acceptable_state[1] + "')")
             else:
@@ -178,19 +191,24 @@ def get_next_token(init_state):
             if token != '' and acceptable_state[0] != 'one_line_comment' and acceptable_state[0] \
                     != 'multi_line_comment':
                 token_list += [(acceptable_state[0], token)]
+                return [(acceptable_state[0], token)]
 
         else:
             if new_state[0] == 'multi_line_comment':
                 error_list += [('/*', 'invalid input')]
+                return [('/*', 'invalid input')]
             else:
                 error_list += [(token, 'invalid input')]
+                return [(token, 'invalid input')]
+    else:
+        return [('EOF', 'EOF')]
 
-        token = ''
-        valid_token = True
-        acceptable_state = [None, ' ']
-        new_state = ['SymNumIdKeySpace', ' ']
 
-    return new_state[0]
+def get_next_token():
+    res = None
+    while res is None:
+        res = make_next_token()
+    return res
 
 
 def output_form_converter(token):
@@ -200,27 +218,7 @@ def output_form_converter(token):
     return output
 
 
-pre_line_state = 'SymNumIdKeySpace'
-for i in range(len(code_string_all)):
-    code_string = code_string_all[i]
-
-    if pre_line_state == 'multi_line_comment':
-        pre_line_state = get_next_token(['multi_line_comment', ' '])
-    else:
-        pre_line_state = get_next_token(['SymNumIdKeySpace', ' '])
-    token_list = output_form_converter(token_list)
-    error_list = output_form_converter(error_list)
-
-    if len(token_list) != 0:
-        scanner_file.write(str(scanner_line_iterator) + '. ' + ' '.join(token_list) + '\n')
-    scanner_line_iterator += 1
-
-    if len(error_list) != 0:
-        error_file.write(str(lexical_error_line_iterator) + '. ' + ' '.join(error_list) + '\n')
-    lexical_error_line_iterator += 1
-    token_list = []
-    error_list = []
-    code_pointer = 0
-
-if pre_line_state == 'multi_line_comment':
-    error_file.write(str(lexical_error_line_iterator) + '. ' + '(invalid comment)')
+token = get_next_token()
+while token != [('EOF', 'EOF')]:
+    scanner_file.write(str(code_line) + str(token[0]) + '\n')
+    token = get_next_token()
